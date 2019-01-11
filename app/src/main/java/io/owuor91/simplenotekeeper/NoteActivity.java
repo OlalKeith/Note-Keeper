17347d21c6a96c51a7be7ef21abbe6a47963778a
Olal Keith <jkeneth.jk@gmail.com>
2019-01-11 06:45:16 +0300

    Creating Navigation Drawer

--- app/src/main/java/io/owuor91/simplenotekeeper/NoteActivity.java
+++ app/src/main/java/io/owuor91/simplenotekeeper/NoteActivity.java
@@ -173,6 +173,14 @@ public class NoteActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuItem item = menu.findItem(R.id.action_next);
+        int lastNoteIndex = DataManager.getInstance().getNotes().size() - 1;
+        item.setEnabled(mNotePosition < lastNoteIndex);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
     private void moveNext() {
         saveNote();
 
@@ -181,6 +189,7 @@ public class NoteActivity extends AppCompatActivity {
 
         saveOriginalNoteValues();
         displayNote(mSpinnerCourses,mTextNoteTitle, mTextNoteText);
+        invalidateOptionsMenu();
 
     }
 
