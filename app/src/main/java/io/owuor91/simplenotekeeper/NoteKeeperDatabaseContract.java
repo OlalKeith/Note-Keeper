cca18ce6f01436c833e771d80ddc90ceb950d924
Olal Keith <jkeneth.jk@gmail.com>
2019-01-24 09:25:10 +0300

    Adding column descriptions to the contract class

--- app/src/main/java/io/owuor91/simplenotekeeper/NoteKeeperDatabaseContract.java
+++ app/src/main/java/io/owuor91/simplenotekeeper/NoteKeeperDatabaseContract.java
@@ -13,8 +13,8 @@ public final class NoteKeeperDatabaseContract {
 
         public static final String SQL_CREATE_TABLE =
                 "CREATE TABLE " + TABLE_NAME + " (" +
-                        COLUMN_COURSE_ID + ", " +
-                        COLUMN_COURSE_TITLE + ")";
+                        COLUMN_COURSE_ID + " TEXT UNIQUE NOT NULL, " +
+                        COLUMN_COURSE_TITLE + " TEXT NOT NULL)";
     }
 
     public static final class NoteInfoEntry {
@@ -25,9 +25,9 @@ public final class NoteKeeperDatabaseContract {
 
         public static final String SQL_CREATE_TABLE =
                 "CREATE TABLE " + TABLE_NAME + " (" +
-                        COLUMN_NOTE_ID + ", " +
-                        COLUMN_NOTE_TITLE + ", " +
-                        COLUMN_NOTE_TEXT + ")";
+                        COLUMN_NOTE_ID + " TEXT NOT NULL, " +
+                        COLUMN_NOTE_TITLE + " TEXT, " +
+                        COLUMN_NOTE_TEXT + " TEXT NOT NULL)";
 
     }
 }
